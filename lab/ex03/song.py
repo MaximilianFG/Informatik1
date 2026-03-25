da0a9c97caf4548467a26646ed670651b94d0b15
PhytalGitHub <goldenphoenx@gmx.de>
2026-03-25 14:40:56 +0100

    test

---
+++ lab/ex03/song.py
@@ -0,0 +1,4 @@
+print("""Heute bin ich digital, ich denke nurnoch an Zahlen.
+      Einsen und Nullen machen meine Träume wahr,
+      die Welt da draussen ist mir egal.
+      (Tim Bendzko: Programmiert. 2013)""")
